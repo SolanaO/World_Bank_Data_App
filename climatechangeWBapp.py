32769f2cf83494e637943b988d473d14e0df2c87
SolanaO <silviaEonofrei@gmail.com>
2021-11-01 21:25:34 -0400

    Add info on how to run app locally.

--- climatechangeWBapp.py
+++ climatechangeWBapp.py
@@ -1,2 +1,6 @@
 from climatechangeWBapp import app
+
+# to run locally, uncheck the line below
+# activate the dashboard virtual environment
+# go to http://0.0.0.0:3001/
 #app.run(host='0.0.0.0', port=3001, debug=True)
